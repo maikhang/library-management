b0df1aa46f0cfd4f06f232c0390c67a6f95b4128
Mai Khang <mhkhang.khmt0117@student.ctuet.edu.vn>
2021-03-11 08:45:20 +0700

    setup mongodb + add admin template

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // MongoDB Service
+        Jenssegers\Mongodb\MongodbServiceProvider::class
 
     ],
 
